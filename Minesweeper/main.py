c76e3bc69977836a1939b7b40032fc6cf79077cf
Ryan Tang <ryantang20061171@gmail.com>
2024-12-18 02:52:50 +0000

    sfx

--- Minesweeper/main.py
+++ Minesweeper/main.py
@@ -1,7 +1,7 @@
 import pygame
 import random
 import sys
-import os
+from pygame import mixer
 from enum import Enum
 from typing import List
 
@@ -14,9 +14,6 @@ NUM_MINES : int
 
 CELL_WIDTH: int = 32
 CELL_HEIGHT: int = 32
-
-WHITE: tuple = (255, 255, 255)
-BLACK: tuple = (0, 0, 0)
 
 DIRECTIONS = (
     (0, -1),
@@ -66,8 +63,8 @@ class Cell:
     def countMines(self) -> int:
         count: int = 0
 
-        x = self.coordinate[0]
-        y = self.coordinate[1]
+        x: int = self.coordinate[0]
+        y: int = self.coordinate[1]
 
         for dir in DIRECTIONS:
             if not outOfBounds(x + dir[0], y + dir[1]):
@@ -77,8 +74,8 @@ class Cell:
     def countFlags(self) -> int:
         count: int = 0
 
-        x = self.coordinate[0]
-        y = self.coordinate[1]
+        x: int = self.coordinate[0]
+        y: int = self.coordinate[1]
 
         for dir in DIRECTIONS:
             if not outOfBounds(x + dir[0], y + dir[1]):
@@ -96,31 +93,37 @@ flags: List[Cell]
 running: bool
 alive : bool
 
-def resource_path(relative_path):
-    try:
-    # PyInstaller creates a temp folder and stores path in _MEIPASS
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
-
-    return os.path.join(base_path, relative_path)
+click: str
+lose: str
+win: str
 
 def initVariables() -> None:
-    global gameStart, tiles_sheet, tiles, safe_tiles, mines, flags, running, alive
+    global tiles_sheet, gameStart, running, alive, tiles, safe_tiles, mines, flags, click, win, lose
+
+    tiles_sheet = pygame.image.load('Assets/tiles.png')
 
     gameStart = False
+    running = True
     alive = True
-    tiles_sheet = pygame.image.load('Assets/tiles.png')
+    
     tiles = []
     safe_tiles = []
     mines = []
     flags = []
-    running = True
+
+    click = 'Assets/click.mp3'  
+    win = 'Assets/win.mp3'
+    lose = 'Assets/explosion.wav'
 
 def setupGame(difficulty: str) -> None:
     global grid, NUM_ROWS, NUM_COLUMNS, NUM_MINES, WIDTH, HEIGHT
+
+    mixer.init() 
+    mixer.music.set_volume(0.7)
+
     mines: int
     size: tuple
+
     if difficulty == 'E':
         size = Difficulty.EASY.value[0]
         mines = Difficulty.EASY.value[1]
@@ -133,29 +136,28 @@ def setupGame(difficulty: str) -> None:
     elif difficulty == 'EX':
         size = Difficulty.EXTREME.value[0]
         mines = Difficulty.EXTREME.value[1]
+    else:
+        raise ValueError("Invalid Difficulty")
 
     NUM_ROWS = size[1]
     NUM_COLUMNS = size[0]
     NUM_MINES = mines
 
     WIDTH = CELL_WIDTH * NUM_COLUMNS    
-    HEIGHT = CELL_HEIGHT * NUM_ROWS   
-
-    position: tuple = ((WIDTH - (NUM_COLUMNS * CELL_WIDTH)) / 2, (HEIGHT - (NUM_ROWS * CELL_HEIGHT)) / 2)
-
-    grid = [[Cell((cell, row), pygame.Rect((cell * CELL_WIDTH) + position[0], (row * CELL_HEIGHT) + position[1], CELL_WIDTH, CELL_HEIGHT)) for cell in range(NUM_COLUMNS)] for row in range(NUM_ROWS)]  
+    HEIGHT = CELL_HEIGHT * NUM_ROWS  
+ 
+    grid = [[Cell((cell, row), pygame.Rect(cell * CELL_WIDTH, row * CELL_HEIGHT, CELL_WIDTH, CELL_HEIGHT)) for cell in range(NUM_COLUMNS)] for row in range(NUM_ROWS)]  
 
     initVariables()
     initDisplay()
 
 def initDisplay() -> None:
     global screen
+
     pygame.init()
     pygame.display.set_caption("Minesweeper")
-    pygame.display.set_icon(pygame.image.load('Assets' + '/icon.webp'))
+    pygame.display.set_icon(pygame.image.load('Assets/icon.webp'))
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    screen.fill(WHITE)
-    pygame.display.flip()
     getTileSprites()
 
 def getTileSprites() -> None:
@@ -168,10 +170,10 @@ def getTileSprites() -> None:
             tiles.append(pygame.transform.scale_by(tile, 2))
 
 def setMines(numMines: int) -> None:
-    for m in range(numMines):
-        random_x = random.randrange(0, NUM_COLUMNS)
-        random_y = random.randrange(0, NUM_ROWS)
-        random_cell = grid[random_y][random_x]
+    for _ in range(numMines):
+        random_x: int = random.randrange(0, NUM_COLUMNS)
+        random_y: int = random.randrange(0, NUM_ROWS)
+        random_cell: Cell = grid[random_y][random_x]
         
         if random_cell.type != 'M' and random_cell not in safe_tiles:
             random_cell.type = 'M'
@@ -179,7 +181,6 @@ def setMines(numMines: int) -> None:
 
 def setGrid() -> None:
     setMines(NUM_MINES)
-
     for row in grid:
         for cell in row:
             if cell.type != 'M':
@@ -192,7 +193,7 @@ def mouseClick(mousePos: tuple, button: int) -> None:
 
     cell = grid[mouse_y][mouse_x]
 
-    if button == 1 and not cell.flagged:
+    if button == 1 and not cell.flagged:   
         if not gameStart:
             firstClick(cell)
             gameStart = True
@@ -201,8 +202,7 @@ def mouseClick(mousePos: tuple, button: int) -> None:
         elif cell.type == 'M':          
             cell.type = '!M'
             endGame()
-        elif cell.type in range(1, 9) and cell.type == cell.countFlags():
-            autoFill((mouse_x, mouse_y))
+        elif cell.type in range(1, 9) and cell.type == cell.countFlags(): autoFill((mouse_x, mouse_y))
         cell.revealed = True
     
     elif button == 3 and not cell.revealed:
@@ -214,48 +214,47 @@ def mouseClick(mousePos: tuple, button: int) -> None:
             flags.pop()
 
 def outOfBounds(x: int, y: int) -> bool:
-    return x < 0 or x >= NUM_COLUMNS or y < 0 or y >= NUM_ROWS
+    return x < 0 or x > NUM_COLUMNS - 1 or y < 0 or y > NUM_ROWS - 1
 
 def firstClick(cell: Cell) -> None:
     cell.type = 0
-    cell_x = cell.coordinate[0]
-    cell_y = cell.coordinate[1]
+    x: int = cell.coordinate[0]
+    y: int = cell.coordinate[1]
 
     for dir in DIRECTIONS:
-        if not outOfBounds(cell_x + dir[0], cell_y + dir[1]):
-            safe_tiles.append(grid[cell_y + dir[1]][cell_x + dir[0]])
+        if not outOfBounds(x + dir[0], y + dir[1]):
+            safe_tiles.append(grid[y + dir[1]][x + dir[0]])
     
-    safe_tiles.append(grid[cell_y][cell_x])
+    safe_tiles.append(grid[y][x])
     setGrid()
 
 def floodFill(coordinate: tuple) -> None:
-    x_cord: int = coordinate[0]
-    y_cord: int = coordinate[1]
-    if outOfBounds(x_cord, y_cord): return
+    x: int = coordinate[0]
+    y: int = coordinate[1]
+    if outOfBounds(x, y): return
 
-    cell = grid[y_cord][x_cord]
+    cell = grid[y][x]
 
     if cell.flagged: return  
     if cell.revealed: return
     if cell.type == 'M': return 
 
-    grid[y_cord][x_cord].revealed = True
+    grid[y][x].revealed = True
 
     if cell.type in range(1, 9): return
 
     for dir in DIRECTIONS:
-        floodFill((x_cord + dir[0], y_cord + dir[1]))
+        floodFill((x + dir[0], y + dir[1]))
 
 def autoFill(coordinate: tuple) -> None:
-    x_cord: int = coordinate[0]
-    y_cord: int = coordinate[1]
+    x: int = coordinate[0]
+    y: int = coordinate[1]
 
     for dir in DIRECTIONS:
-        if not outOfBounds(x_cord + dir[0], y_cord + dir[1]):
-            cell = grid[y_cord + dir[1]][x_cord + dir[0]]
+        if not outOfBounds(x + dir[0], y + dir[1]):
+            cell = grid[y + dir[1]][x + dir[0]]
             if not cell.flagged:
-                if cell.type == 0:
-                    floodFill((x_cord + dir[0], y_cord + dir[1]))
+                if cell.type == 0: floodFill((x + dir[0], y + dir[1]))
                 cell.revealed = True
             if cell.type == 'M' and not cell.flagged:
                 cell.type = '!M'
@@ -276,9 +275,16 @@ def updateBoard() -> None:
 def printBoard() -> None:
     for row in grid:
         print(" ".join(f"{str(cell.type):<2}" for cell in row))
+
+def gameWin() -> bool:
+    for row in grid:
+        for cell in row:
+            if cell.type in range(9) and not cell.revealed: return False
+    return True
    
 def endGame() -> None:
     global alive
+
     for cell in flags:
         if cell.flagged and cell.type != 'M':
             cell.flagged = False
@@ -290,8 +296,12 @@ def endGame() -> None:
 
     alive = False
 
+    pygame.mixer.music.load(lose)
+    mixer.music.play()
+
 def main() -> None:
-    global running
+    global running, alive
+
     while running:
         mousePos = pygame.mouse.get_pos()
 
@@ -309,9 +319,13 @@ def main() -> None:
  
             if event.type == pygame.MOUSEBUTTONUP and alive:
                 mouseClick(mousePos, event.button)
+                if gameWin() and alive:
+                    alive = False
+                    pygame.mixer.music.load(win)
+                    mixer.music.play()
 
         updateBoard()
       
 if __name__ == '__main__':
-    setupGame('EX')
+    setupGame('M')
     main()
